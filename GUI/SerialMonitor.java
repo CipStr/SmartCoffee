e7d410c70d2f29a6f7aad5fb485b0474a9fe5635
CavDev26 <lorenzo.cavallucci26@gmail.com>
2022-05-10 16:21:50 +0200

    Firsst try to fix errorrs in GUI

--- GUI/SerialMonitor.java
+++ GUI/SerialMonitor.java
@@ -1,85 +1,85 @@
-//package esiot.module_lab_2_2;
+// //package esiot.module_lab_2_2;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import jssc.*;
+// import java.io.BufferedReader;
+// import java.io.InputStreamReader;
+// import jssc.*;
 
-public class SerialMonitor implements SerialPortEventListener {
-	SerialPort serialPort;
+// public class SerialMonitor implements SerialPortEventListener {
+// 	SerialPort serialPort;
 	
-	public void start(String portName) {
-		serialPort = new SerialPort(portName);
-		try {
-		    serialPort.openPort();
+// 	public void start(String portName) {
+// 		serialPort = new SerialPort(portName);
+// 		try {
+// 		    serialPort.openPort();
 
-		    serialPort.setParams(SerialPort.BAUDRATE_9600,
-		                         SerialPort.DATABITS_8,
-		                         SerialPort.STOPBITS_1,
-		                         SerialPort.PARITY_NONE);
+// 		    serialPort.setParams(SerialPort.BAUDRATE_9600,
+// 		                         SerialPort.DATABITS_8,
+// 		                         SerialPort.STOPBITS_1,
+// 		                         SerialPort.PARITY_NONE);
 
-		    serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN | 
-		                                  SerialPort.FLOWCONTROL_RTSCTS_OUT);
+// 		    serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN | 
+// 		                                  SerialPort.FLOWCONTROL_RTSCTS_OUT);
 
-		    serialPort.addEventListener(this, SerialPort.MASK_RXCHAR);
-		}
-		catch (SerialPortException ex) {
-		    System.out.println("There are an error on writing string to port т: " + ex);
-		}
+// 		    serialPort.addEventListener(this, SerialPort.MASK_RXCHAR);
+// 		}
+// 		catch (SerialPortException ex) {
+// 		    System.out.println("There are an error on writing string to port т: " + ex);
+// 		}
 		
-		new UserInputReaderAgent(serialPort).start();
-	}
+// 		new UserInputReaderAgent(serialPort).start();
+// 	}
 
-	/**
-	 * This should be called when you stop using the port.
-	 * This will prevent port locking on platforms like Linux.
-	 */
-	public synchronized void close() {
-		try {
-			if (serialPort != null) {
-				serialPort.removeEventListener();
-				serialPort.closePort();
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+// 	/**
+// 	 * This should be called when you stop using the port.
+// 	 * This will prevent port locking on platforms like Linux.
+// 	 */
+// 	public synchronized void close() {
+// 		try {
+// 			if (serialPort != null) {
+// 				serialPort.removeEventListener();
+// 				serialPort.closePort();
+// 			}
+// 		} catch (Exception ex) {
+// 			ex.printStackTrace();
+// 		}
+// 	}
 
-	/**
-	 * Handle an event on the serial port. Read the data and print it.
-	 */
-	@Override
-	public synchronized void serialEvent(SerialPortEvent event) {
-        if(event.isRXCHAR() && event.getEventValue() > 0) {
-            try {
-                String receivedData = serialPort.readString(event.getEventValue());
-                System.out.print(receivedData);
-            }
-            catch (SerialPortException ex) {
-                System.out.println("Error in receiving string from COM-port: " + ex);
-            }
-        }
-	}
+// 	/**
+// 	 * Handle an event on the serial port. Read the data and print it.
+// 	 */
+// 	@Override
+// 	public synchronized void serialEvent(SerialPortEvent event) {
+//         if(event.isRXCHAR() && event.getEventValue() > 0) {
+//             try {
+//                 String receivedData = serialPort.readString(event.getEventValue());
+//                 System.out.print(receivedData);
+//             }
+//             catch (SerialPortException ex) {
+//                 System.out.println("Error in receiving string from COM-port: " + ex);
+//             }
+//         }
+// 	}
 
-	class UserInputReaderAgent extends Thread {
+// 	class UserInputReaderAgent extends Thread {
 		
-		private SerialPort port;
+// 		private SerialPort port;
 
-		public UserInputReaderAgent(SerialPort port){
-			this.port = port;
-		}
+// 		public UserInputReaderAgent(SerialPort port){
+// 			this.port = port;
+// 		}
 		
-		public void run(){
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			while (true){
-				try {
-					String st = reader.readLine();
-					port.writeString(st,"UTF-8");
-				} catch (Exception ex){
-					ex.printStackTrace();
-				}
-			}
-		}
-	}
+// 		public void run(){
+// 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+// 			while (true){
+// 				try {
+// 					String st = reader.readLine();
+// 					port.writeString(st,"UTF-8");
+// 				} catch (Exception ex){
+// 					ex.printStackTrace();
+// 				}
+// 			}
+// 		}
+// 	}
 
 
-}
+// }
